336a69c9608cba85bdab9918f492ea033c16fec6
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-09-28 12:11:16 -0300

    Adding angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
