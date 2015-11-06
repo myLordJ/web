28355489463f93d331e71c8d1fe95f9ceec08141
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-06 16:56:44 -0300

    Adding player

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -3,8 +3,13 @@
 angular.module('rac')
 .controller('HeaderCtrl', function ($scope, $weather) {
   $scope.temp = {};
+  $scope.isPlaying = false;
 
   $weather.get({}, function(city){
     $scope.temp = city.main;
   });
+
+  $scope.play = function() {
+    $scope.isPlaying = !$scope.isPlaying;
+  };
 });
