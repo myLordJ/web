8d49afd3912b36494ea54ea518d5049451f2bdd1
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-06 15:25:44 -0300

    Adding header controller

--- app/scripts/services/weather.js
+++ app/scripts/services/weather.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('rac')
   .factory('$weather', function($resource) {
-    return $resource('http://api.openweathermap.org/data/2.5/weather?id=3433955&appid=c51971bedd25383fc22e9e2846e868b1', {}, 
+    return $resource('http://api.openweathermap.org/data/2.5/weather?id=3433955&units=metric&appid=c51971bedd25383fc22e9e2846e868b1', {}, 
       { 
         get: { 
           method: 'GET'
