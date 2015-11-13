6f6583eaebae13082c9db0ad82d3dcca9def6acb
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-12 23:02:35 -0300

    Adding mixclouds to home

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('rac')
-  .controller('HomeCtrl', function ($scope, $gallery, $page, $sce) {
+  .controller('HomeCtrl', function ($scope, $gallery, $page, $sce, $mixcloud) {
     $scope.images = [];
+    $scope.mixcloud = [];
     $scope.interval = 3000;
 
     $page.get({name: 'home'}, function(page) {
@@ -12,5 +13,9 @@ angular.module('rac')
     $gallery.get({key: 'home-page-slider'}, function(gallery) {
       gallery[0].active = true;
       $scope.images = gallery;
+    });
+
+    $mixcloud.get({}, function(res) {
+      $scope.mixcloud = res.data.slice(0, 4);
     });
   });
