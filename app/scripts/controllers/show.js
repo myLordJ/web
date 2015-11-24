641ebdd1e1f5fe2e54d654dc43f72a44c3a02f9b
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-24 19:56:28 -0300

    Adding show verification

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -3,4 +3,9 @@
 angular.module('rac')
   .controller('ShowCtrl', function ($scope, $location, $current) {
     $scope.show = $current.show;
+
+    if (!$scope.show) {
+      //TODO: If no post in memory request it to server.
+      $location.path('/schedule');
+    }
   });
