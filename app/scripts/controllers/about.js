aa0e6f660f9f94fd6290680baa7066c07fc46aeb
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-27 19:36:51 -0300

    Adding workspace gallery to about

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('rac')
-  .controller('AboutCtrl', function ($scope, $sce, $page) {
+  .controller('AboutCtrl', function ($scope, $sce, $page, $gallery) {
     $scope.title = 'Nosotros';
 
     $page.get({name: 'about'}, function(page) {
       $scope.content = $sce.trustAsHtml(page.content);
       $scope.title = page.title;
       $scope.subTitle = page.subTitle;
+    });
+
+    $gallery.get({key: 'workspace'}, function(gallery) {
+      $scope.images = gallery;
     });
   });
