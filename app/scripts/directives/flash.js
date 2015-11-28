4fdeb75cc8f27e37b95f65e8566821bf86ef244b
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-11-28 16:09:57 -0300

    Adding flash support directive

---
+++ app/scripts/directives/flash.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('rac.directives')
+.directive('isFlash', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var isFlash = (attrs.isFlash === 'true');
+      var isFashInstalled = ((typeof navigator.plugins !== 'undefined' && typeof navigator.plugins['Shockwave Flash'] === 'object') || (window.ActiveXObject && (new ActiveXObject('ShockwaveFlash.ShockwaveFlash')) !== false));
+      
+      if (isFashInstalled && isFlash) {
+        $(element.html(attrs.script)).show();
+      }
+      else {
+        $(element.html(attrs.script)).hide();
+      }
+    }
+  };
+});
