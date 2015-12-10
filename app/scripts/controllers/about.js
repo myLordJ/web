13697284a82757f7155e9fa00185fdb5dbe3e764
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-12-09 23:46:45 -0300

    Adding team members

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -13,4 +13,10 @@ angular.module('rac')
     $gallery.get({name: 'workspace'}, function(gallery) {
       $scope.images = gallery;
     });
+
+    $scope.users = [
+      {name: 'Juan Gnarra', job: 'CEO', twitter: '#', facebook: '#', profilePicture: '../new/images/team-1.jpg'},
+      {name: 'Agustin Peyloubet', job: 'COO', twitter: '#', facebook: '#', profilePicture: '../new/images/team-1.jpg'},
+      {name: 'El Jota', job: 'CTO', twitter: '#', facebook: '#', profilePicture: '../new/images/team-1.jpg'}
+    ];
   });
