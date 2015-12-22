65631fd2511c60d5c01c2103748336f6944f6568
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-12-22 10:02:54 -0300

    Removing all events

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -13,53 +13,6 @@ angular.module('rac')
         $location.path('/show/' + $current.show.title);
     };
 
-    $scope.events = [
-      {
-        title: 'Perros de la Calle',
-        type: 'info',
-        startsAt: new Date(2015,1,1,10),
-        endsAt: new Date(2016,1,1,10),
-        editable: false,
-        deletable: false,
-        draggable: false, 
-        resizable: true,
-        recursOn: 'month'
-      },
-      {
-        title: 'Basta de Todo',
-        type: 'success',
-        startsAt: new Date(2015,1,1,14),
-        endsAt: new Date(2016,1,1,14),
-        editable: false,
-        deletable: false,
-        draggable: false, 
-        resizable: true,
-        recursOn: 'month'
-      },
-      {
-        title: 'No Somos Nadie',
-        type: 'warning',
-        startsAt: new Date(2015,1,1,6),
-        endsAt: new Date(2016,1,1,10),
-        editable: false,
-        deletable: false,
-        draggable: false, 
-        resizable: true,
-        recursOn: 'month'
-      },
-      {
-        title: 'Metro y Medio',
-        type: 'inverse',
-        startsAt: new Date(2015,1,1,18),
-        endsAt: new Date(2016,1,1,18),
-        editable: false,
-        deletable: false,
-        draggable: false, 
-        resizable: true,
-        recursOn: 'month'
-      },
-    ];
-
     $page.get({name: 'schedule'}, function(page) {
       $scope.title = page.title;
       $scope.subTitle = page.subTitle;
