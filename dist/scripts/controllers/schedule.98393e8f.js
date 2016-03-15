ed0001b81f87a0d3c5c1fca7983fc933dd1b605c
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-03-15 14:52:32 -0300

    Running build

---
+++ dist/scripts/controllers/schedule.98393e8f.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('rac')
+  .controller('ScheduleCtrl', function ($scope, $rootScope, $mixcloud, $page, $show, $current, $location) {
+    $scope.calendarView = 'month';
+    $scope.currentDay = new Date();
+    $scope.title = 'Programación';
+    $scope.subTitle = '';
+
+    $scope.shows = [];
+
+    $scope.goToShow = function(){//index) {
+      return;
+      //$current.show = this.shows[index];
+      //$location.path('/show/' + $current.show.title);
+    };
+
+    $page.get({name: 'schedule'}, function(page) {
+      $rootScope.title = page.title;
+      $rootScope.subTitle = page.subTitle;
+    });
+
+    $show.get({}, function(shows) {
+        $scope.shows = shows;
+        $scope.loadedImages = true;
+    });
+
+  });
