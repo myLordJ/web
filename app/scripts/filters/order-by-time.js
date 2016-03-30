c650c7f3baa192235df6ab663011f22ba04afa1c
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-03-30 17:49:17 -0300

    Setting order values

--- app/scripts/filters/order-by-time.js
+++ app/scripts/filters/order-by-time.js
@@ -1,20 +1,46 @@
 'use strict';
 
 angular.module('rac.filters')
-  .filter('orderByTime', function (items) {
+  .filter('orderByTime', function () {
     function getOrder(item) {
-      console.log(item);
+      if (item.monday) {
+        return parseInt(1 + item.mondayStartTime.toString());
+      }
+
+      if (item.tuesday) {
+        return parseInt(2 + item.tuesdayStartTime.toString());
+      }
+
+      if (item.wednesday) {
+        return parseInt(3 + item.wednesdayStartTime.toString());
+      }
+
+      if (item.thursday) {
+        return parseInt(4 + item.thursdayStartTime.toString());
+      }
+
+      if (item.friday) {
+        return parseInt(5 + item.fridayStartTime.toString());
+      }
+
+      if (item.saturday) {
+        return parseInt(6 + item.saturdayStartTime.toString());
+      }
+
+      if (item.sunday) {
+        return parseInt(7 + item.sundayStartTime.toString());
+      }
     }
 
-    return function() {
+    return function(items) {
       var filtered = [];
       angular.forEach(items, function(item) {
         filtered.push(item);
       });
-      filtered.sort(function (a, b) {    
+      filtered.sort(function (a, b) {
         return (getOrder(a) > getOrder(b) ? 1 : -1);
       });
-      
+
       return filtered;
     };
   });
