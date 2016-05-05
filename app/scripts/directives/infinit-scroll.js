faa8dba9da181f17d9ccbe91c31f2dae376d3217
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-05 10:09:54 -0300

    Formatting code

--- app/scripts/directives/infinit-scroll.js
+++ app/scripts/directives/infinit-scroll.js
@@ -5,7 +5,6 @@ angular.module('rac.directives')
   return function(scope, element, attrs) {
     angular.element($window).bind('scroll', function() {
       if ($(window).scrollTop() === $(document).height() - $(window).height()) {
-      //if($(element).scrollTop() + $(element).innerHeight() >= $(element)[0].scrollHeight){
         scope.$apply(attrs.infiniteScroll);
       }
     });
