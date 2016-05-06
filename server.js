6ddcadf2d7ec375a0df16af478c24f819d105ddc
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-05 23:46:46 -0300

    Formatting server

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// OPBEAT_APPID=a100223e18
-// OPBEAT_ORGID=707bc536d3794808a2d89bec776dac46
-// OPBEAT_TOKEN=b560dd51115180f17322ae8776aaa2deba619a69
 var opbeat = require('opbeat').start({
   appId: process.env.OPBEAT_APPID,
   organizationId: process.env.OPBEAT_ORGID,
