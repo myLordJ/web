1d1a78a8bc0f0c49035671fb2b65884285852761
Ignacio Anaya <ignacio.anaya89@gmail.com>
2016-05-05 23:44:27 -0300

    Adding opbeat

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+// OPBEAT_APPID=a100223e18
+// OPBEAT_ORGID=707bc536d3794808a2d89bec776dac46
+// OPBEAT_TOKEN=b560dd51115180f17322ae8776aaa2deba619a69
+var opbeat = require('opbeat').start({
+  appId: process.env.OPBEAT_APPID,
+  organizationId: process.env.OPBEAT_ORGID,
+  secretToken: process.env.OPBEAT_TOKEN
+});
+
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
